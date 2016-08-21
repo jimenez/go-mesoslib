bd42c546b7a378b1aa0f2c982d9e9a8f8f8ebe17
Isabel Jimenez <contact.isabeljimenez@gmail.com>
2016-08-21 08:13:32 +0200

    good uuid lib

--- executor/update.go
+++ executor/update.go
@@ -1,33 +1,12 @@
 package executor
 
 import (
-	"crypto/rand"
-	"fmt"
-	"io"
-
 	"github.com/jimenez/go-mesoslib/mesosproto"
 	"github.com/jimenez/go-mesoslib/mesosproto/executorproto"
+	"github.com/pborman/uuid"
 )
 
-// newUUID generates a random UUID according to RFC 4122
-func newUUID() (string, error) {
-	uuid := make([]byte, 16)
-	n, err := io.ReadFull(rand.Reader, uuid)
-	if n != len(uuid) || err != nil {
-		return "", err
-	}
-	// variant bits; see section 4.1.1
-	uuid[8] = uuid[8]&^0xc0 | 0x80
-	// version 4 (pseudo-random); see section 4.1.3
-	uuid[6] = uuid[6]&^0xf0 | 0x40
-	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
-}
-
 func (lib *ExecutorLib) update(task *mesosproto.TaskInfo, state *mesosproto.TaskState) error {
-	// uuid, err := newUUID()
-	// if err != nil {
-	// 	return err
-	// }
 	call := &executorproto.Call{
 		Type:        executorproto.Call_UPDATE.Enum(),
 		FrameworkId: lib.frameworkID,
@@ -36,7 +15,7 @@ func (lib *ExecutorLib) update(task *mesosproto.TaskInfo, state *mesosproto.Task
 			Status: &mesosproto.TaskStatus{
 				TaskId: task.GetTaskId(),
 				State:  state,
-				Uuid:   []byte("0123456789abcdef0123456789abcdef"),
+				Uuid:   uuid.NewUUID(),
 			},
 		},
 	}
